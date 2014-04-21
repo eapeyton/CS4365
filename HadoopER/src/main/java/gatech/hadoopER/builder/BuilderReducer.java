0394a73e51acfdb78447f77a571bf8e2f4e2be54
Eric <ea.peyton@gmail.com>
2014-04-21 19:17:22 -0400

    Updating repo

--- HadoopER/src/main/java/gatech/hadoopER/builder/BuilderReducer.java
+++ HadoopER/src/main/java/gatech/hadoopER/builder/BuilderReducer.java
@@ -6,7 +6,10 @@ package gatech.hadoopER.builder;
 
 import gatech.hadoopER.importer.To;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableUtils;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.log4j.Logger;
 
@@ -27,10 +30,14 @@ public class BuilderReducer extends Reducer<Text,To,To,To> {
     protected void reduce(Text key, Iterable<To> values, Context context) throws IOException, InterruptedException {
         int i=0;
         Logger.getLogger(this.getClass()).info("Key: " + key.toString());
+        List<To> cloned = new ArrayList<>();
         for(To value: values) {
+            cloned.add(WritableUtils.clone(value, context.getConfiguration()));
+        }
+        for(To value: cloned) {
             Logger.getLogger(this.getClass()).info("Value: " + value.toString());
             int j=0;
-            for(To otherValue: values) {
+            for(To otherValue: cloned) {
                 if(j > i) {
                     if(builder.areMatching(value, otherValue)) {
                         context.write(value, otherValue);
