924e4273e1568a5159e8b6410bb6662f27e01241
Eric <ea.peyton@gmail.com>
2014-04-23 04:19:26 -0400

    Updating repo

--- HadoopER/src/main/java/gatech/hadoopER/products/GlobalProduct.java
+++ HadoopER/src/main/java/gatech/hadoopER/products/GlobalProduct.java
@@ -6,6 +6,7 @@ package gatech.hadoopER.products;
 import gatech.hadoopER.StringSet;
 import gatech.hadoopER.importer.To;
 import gatech.hadoopER.util.ERUtil;
+import java.util.Objects;
 import java.util.Set;
 import org.apache.hadoop.io.ArrayWritable;
 
@@ -14,13 +15,6 @@ import org.apache.hadoop.io.ArrayWritable;
  * @author eric
  */
 public class GlobalProduct extends To {
-
-    ;
-
-    @Override
-    public String toString() {
-        return "GlobalProduct{" + "id=" + id + ", name=" + name + ", description=" + description + ", manufacturer=" + manufacturer + ", price=" + price + '}';
-    }
 
     StringSet id = new StringSet();
     StringSet name = new StringSet();
@@ -38,6 +32,49 @@ public class GlobalProduct extends To {
         public GPArrayWritable() {
             super(GlobalProduct.class);
         }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.id);
+        hash = 41 * hash + Objects.hashCode(this.name);
+        hash = 41 * hash + Objects.hashCode(this.description);
+        hash = 41 * hash + Objects.hashCode(this.manufacturer);
+        hash = 41 * hash + (int) (Double.doubleToLongBits(this.price) ^ (Double.doubleToLongBits(this.price) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GlobalProduct other = (GlobalProduct) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.manufacturer, other.manufacturer)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.price) != Double.doubleToLongBits(other.price)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "GlobalProduct{" + "id=" + id + ", name=" + name + ", description=" + description + ", manufacturer=" + manufacturer + ", price=" + price + '}';
     }
 
 }
--- HadoopER/src/main/java/gatech/hadoopER/products/ImportAmazon.java
+++ HadoopER/src/main/java/gatech/hadoopER/products/ImportAmazon.java
@@ -40,7 +40,7 @@ public class ImportAmazon extends ImporterCSV<AmazonProduct, GlobalProduct> {
 
     @Override
     public Path getInputPath() {
-        return new Path("/user/epeyton.site/products/input/");
+        return new Path("/user/epeyton.site/products/input-big/");
     }
 
     public static class AmazonProduct extends From {
